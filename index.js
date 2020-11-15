42dac5feb91c9324820afb31d38dfbb2d94ec8df
unknown <rpl1official@gmail.com>
2020-11-16 02:49:14 +0700

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://botler-2-0.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
